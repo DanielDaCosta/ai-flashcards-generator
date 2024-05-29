0a466daf7773708fa726c2bd3804cc186199e3eb
Snow Storm <daniel.pereiracosta@hotmail.com>
2024-05-29 10:12:50 -0700

    Add middleware

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, HttpUrl
+from fastapi.middleware.cors import CORSMiddleware
 
 class VideoAnalysisRequest(BaseModel):
-    youtube_lik: HttpUrl
+    youtube_link: HttpUrl
     # advanced settings
 
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.post("/analyze_video")
 def analyze_video(request: VideoAnalysisRequest):
@@ -13,7 +23,7 @@ def analyze_video(request: VideoAnalysisRequest):
     from langchain_community.document_loaders import YoutubeLoader
     from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-    loader = YoutubeLoader.from_youtube_url(str(request.youtube_lik), add_video_info=True)
+    loader = YoutubeLoader.from_youtube_url(str(request.youtube_link), add_video_info=True)
     docs = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     result = text_splitter.split_documents(docs)
